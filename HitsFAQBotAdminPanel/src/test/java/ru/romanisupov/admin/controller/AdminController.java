e2e5ac07cf0292f0af189f5b89ac210df49759ef
RomanIsupov <romisup@gmail.com>
2022-06-19 16:15:34 +0800

    Add controller layer
    I Have added controller layer with undone (yet) methods.
    Also I will add different controller for authentication
    once I have decided what way I would do it.

---
+++ HitsFAQBotAdminPanel/src/test/java/ru/romanisupov/admin/controller/AdminController.java
@@ -0,0 +1,25 @@
+package ru.romanisupov.controller;
+
+import ru.romanisupov.entity.Question;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/questions")
+public class AdminController {
+
+	@GetMapping
+	public List<Question> getQuestions() {
+		//TODO complete method
+	}
+
+	@PostMapping
+	public void answerQuestion(@Valid Question) {
+		//TODO complete method
+	}
+}
